db8f22c4994326352b58f8def20df281ecf8c1cd
ell-ska <ella@skagerstrom.se>
2023-10-06 15:01:12 +0200

    base setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,17 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      primary: 'var(--font-lexend)',
+      branding: 'var(--font-climate-crisis)',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#1ED760',
       },
     },
   },
   plugins: [],
 }
+
 export default config

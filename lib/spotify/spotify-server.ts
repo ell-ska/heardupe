1d0feed101424121835d0cb177f26f47f04469d0
ell-ska <ella@skagerstrom.se>
2024-05-03 21:47:02 +0200

    fix: remove type

--- lib/spotify/spotify-server.ts
+++ lib/spotify/spotify-server.ts
@@ -1,19 +1,19 @@
 import { AccessToken, SpotifyApi } from '@spotify/web-api-ts-sdk'
 
-import { auth } from '@/auth'
-import { ExtendedUser } from '@/types'
-
+import { auth, signIn } from '@/auth'
 const clientID = process.env.SPOTIFY_CLIENT_ID || ''
 
 const getServerSdk = async () => {
   const session = await auth()
-  const user = session?.user as ExtendedUser
+  const user = session?.user
+
+  if (!user) return signIn()
 
   const accessToken: AccessToken = {
-    access_token: user?.access_token,
-    token_type: user?.token_type,
-    expires_in: user?.expires_in,
-    refresh_token: user?.refresh_token,
+    access_token: user.access_token,
+    token_type: user.token_type,
+    expires_in: user.expires_in,
+    refresh_token: user.refresh_token,
   }
 
   return SpotifyApi.withAccessToken(clientID, accessToken)

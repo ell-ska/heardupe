4d0563c70ce4885ded2ee500f7d2c1976baa810b
ell-ska <ella@skagerstrom.se>
2024-06-30 19:26:25 +0200

    fix: migrate to v7

--- actions/become-beta-user.ts
+++ actions/become-beta-user.ts
@@ -6,25 +6,27 @@ import { db } from '@/lib/db'
 import { action } from '@/actions/utils/safe-action'
 import { becomeBetaUserSchema as schema } from '@/actions/utils/schemas'
 
-export const becomeBetaUser = action(schema, async data => {
-  const existingUser = await db.profile.findUnique({
-    where: { email: data.email },
-  })
-  if (existingUser) throw Error('You already have an account!')
-
-  const alreadyRequested = await db.betaUserRequest.findUnique({
-    where: { email: data.email },
-  })
-  if (alreadyRequested) throw Error('Your request has already been recived!')
-
-  try {
-    await db.betaUserRequest.create({
-      data,
+export const becomeBetaUser = action
+  .schema(schema)
+  .action(async ({ parsedInput: { email, name } }) => {
+    const existingUser = await db.profile.findUnique({
+      where: { email: email },
     })
-  } catch (error) {
-    console.error('[BECOME_BETA_USER_ACTION_ERROR]', error)
-    throw Error('Something went wrong when signing up for the waitlist')
-  }
+    if (existingUser) throw Error('You already have an account!')
 
-  redirect('/become-beta-user/submitted')
-})
+    const alreadyRequested = await db.betaUserRequest.findUnique({
+      where: { email: email },
+    })
+    if (alreadyRequested) throw Error('Your request has already been recived!')
+
+    try {
+      await db.betaUserRequest.create({
+        data: { email, name },
+      })
+    } catch (error) {
+      console.error('[BECOME_BETA_USER_ACTION_ERROR]', error)
+      throw Error('Something went wrong when signing up for the waitlist')
+    }
+
+    redirect('/become-beta-user/submitted')
+  })
--- actions/utils/safe-action.ts
+++ actions/utils/safe-action.ts
@@ -1,8 +1,11 @@
-import { DEFAULT_SERVER_ERROR, createSafeActionClient } from 'next-safe-action'
+import {
+  createSafeActionClient,
+  DEFAULT_SERVER_ERROR_MESSAGE,
+} from 'next-safe-action'
 
 const handleReturnedServerError = (error: Error) => {
   if (error.message) return error.message
-  return DEFAULT_SERVER_ERROR
+  return DEFAULT_SERVER_ERROR_MESSAGE
 }
 
 export const action = createSafeActionClient({ handleReturnedServerError })

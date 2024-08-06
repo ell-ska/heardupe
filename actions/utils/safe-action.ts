9c3ec013cef5e5f01b41a97f8534cfddefec200b
ell-ska <ella@skagerstrom.se>
2024-08-06 11:10:57 +0200

    refactor: improve error handling in become beta user action

--- actions/utils/safe-action.ts
+++ actions/utils/safe-action.ts
@@ -3,6 +3,13 @@ import {
   DEFAULT_SERVER_ERROR_MESSAGE,
 } from 'next-safe-action'
 
+export class ActionError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'ActionError'
+  }
+}
+
 const handleReturnedServerError = (error: Error) => {
   if (error.message) return error.message
   return DEFAULT_SERVER_ERROR_MESSAGE

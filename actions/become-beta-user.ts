3b14f0872bed66d8a19e028db29d321decef50ab
Ella <96110484+ell-ska@users.noreply.github.com>
2024-08-06 12:39:39 +0200

    refactor: improve error handling (#177)

--- actions/become-beta-user.ts
+++ actions/become-beta-user.ts
@@ -3,29 +3,44 @@
 import { redirect } from 'next/navigation'
 
 import { db } from '@/lib/db'
-import { action } from '@/actions/utils/safe-action'
-import { becomeBetaUserSchema as schema } from '@/actions/utils/schemas'
+import { ActionError, action } from '@/actions/utils/safe-action'
+import { becomeBetaUserSchema } from '@/actions/utils/schemas'
 
 export const becomeBetaUser = action
-  .schema(schema)
+  .schema(becomeBetaUserSchema)
   .action(async ({ parsedInput: { email, name } }) => {
-    const existingUser = await db.profile.findUnique({
-      where: { email: email },
-    })
-    if (existingUser) throw Error('You already have an account!')
-
-    const alreadyRequested = await db.betaUserRequest.findUnique({
-      where: { email: email },
-    })
-    if (alreadyRequested) throw Error('Your request has already been recived!')
-
     try {
+      const existingUser = await db.profile.findUnique({
+        where: { email },
+      })
+      if (existingUser) {
+        throw new ActionError(
+          'You already have an account! Try logging in instead.',
+        )
+      }
+
+      const alreadyRequested = await db.betaUserRequest.findUnique({
+        where: { email },
+      })
+      if (alreadyRequested && !alreadyRequested.inDashboard) {
+        throw new ActionError('Your request has already been recived!')
+      }
+      if (alreadyRequested && alreadyRequested.inDashboard) {
+        throw new ActionError(
+          'Your request has been approved! Try logging in instead.',
+        )
+      }
+
       await db.betaUserRequest.create({
         data: { email, name },
       })
     } catch (error) {
+      if (error instanceof ActionError) throw error
+
       console.error('[BECOME_BETA_USER_ACTION_ERROR]', error)
-      throw Error('Something went wrong when signing up for the waitlist')
+      throw new Error(
+        'Something went wrong when signing up for the waitlist, please try again later.',
+      )
     }
 
     redirect('/become-beta-user/submitted')
